ecf1448b4923680e88ee63321a36629c1e587155
Angela Perez <anfperez@gmail.com>
2016-03-03 19:35:39 -0500

    tried upvotes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
 	def create
-    @haunt = Haunt.find(params[:haunt_id])
-    @comment = @haunt.comments.create(params[:comment])
-    redirect_to haunt_path(@haunt)
-  end
+	    @haunt = Haunt.find(params[:haunt_id])
+	    @comment = @haunt.comments.create(comments_params)
+	    redirect_to haunt_path(@haunt)
+    end
+
+    private
+
+    def comments_params
+    	params.require(:comment).permit(:commenter, :body)
+    end
 end
